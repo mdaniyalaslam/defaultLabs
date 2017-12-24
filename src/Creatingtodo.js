ea5ce62b14c1a35b2767aad9cc91ca7b6a7cfeae
Muhammad Daniyal <muhammad.8425450@talmeez.pk>
2017-12-25 01:29:10 +0500

    adding done

--- src/Creatingtodo.js
+++ src/Creatingtodo.js
@@ -16,7 +16,20 @@ class Creatingtodo extends Component {
     }
     this.todoHandler = this.todoHandler.bind(this)
     this.btnHandler = this.btnHandler.bind(this)
+
+    firebase.database().ref('/').child("reacttodos").on('child_added', (snap) => {
+      var obj = snap.val();
+      obj.id = snap.key;
+      this.state.todos.push(obj)
+      this.setState({ todos: this.state.todos });
+      // console.log(this.state.todos)
+    })
   }
+
+
+
+
+
 
   todoHandler(ev) {
     this.setState({
@@ -26,48 +39,45 @@ class Creatingtodo extends Component {
   }
 
   btnHandler(ev) {
-    let currentList = this.state.todos;
-    currentList.push(this.state.todoInput);
-    this.setState({
-      todos: currentList,
-      todoInput: ''
-    })
-    let todos = this.state.todoInput
-    firebase.database().ref('/').child('reacttodos').push(todos)
-    console.log(todos)
+    let todo = this.state.todoInput
+    firebase.database().ref('/').child('reacttodos').push(todo)
+    // console.log(todo)
+    this.setState({todoInput:''})
   }
 
   render() {
     return (
 
       // <div className ="row"></div>
-        <div className="App">
+      <div className="App">
         {/* Header starts */}
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">React Todo</h1>
-          </header>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React Todo</h1>
+        </header>
         {/* Header ends */}
 
-          <div className="col-sm-3 back ">
+        <div className="col-sm-3 back ">
           {/* Todo input area Starts */}
           <h1>Creating Todos</h1>
-            <input type="text"
-              className="form-control field"
-              placeholder="Write Todo"
-              name="todo" value={this.state.todoInput}
-              onChange={this.todoHandler}
-              />
-            <button type="button" className="btn btn-primary" onClick={this.btnHandler}>Add</button>
-            <ul>
-              {this.state.todos.map((val, ind) => {
-                return <li key={ind}> {val} {<button>Button</button>} </li>
-              })}
-            </ul>
+          <input type="text"
+            className="form-control field"
+            placeholder="Write Todo"
+            name="todo" value={this.state.todoInput}
+            onChange={this.todoHandler}
+          />
+          <button type="button" className="btn btn-primary" onClick={this.btnHandler}>Add</button>
+          <ul>
+            {this.state.todos.map((val, ind) => {
+              return <li key={ind}>
+                   {val.todo}{<button>Remove</button>}
+                </li>
+            })}
+          </ul>
           {/* Todo input area Ends */}
-            
-          </div>
+
         </div>
+      </div>
     );
   }
 }
